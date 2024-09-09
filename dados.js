b2f3e2e71df27ec7f7e5d735af813ced1e8db043
GiilsON91 <gilson@prisfood.com.br>
2024-09-09 15:17:09 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo : "Box Antivazamento",
+        descricao : "A melhor opção para suas refeições. As Box Antivazamento da Pris Embalagens são a solução perfeita para o seu serviço de delivery. Este produto combina sustentabilidade, versatilidade e segurança para proporcionar a melhor experiência tanto para você quanto para seus clientes. Feitas de material biodegradável, são uma excelente alternativa ao plástico e ao isopor, contribuindo para a preservação do meio ambiente. Opte por uma solução eco-friendly e promova práticas sustentáveis em seu negócio.Escolha entre os modelos com design padrão ou branco.",
+        link : "https://prisembalagens.com.br/produtos/box-antivazamento/", 
+        tags :  "antivazamento box multiuso foodservice"
+    
+    },
+    {
+        titulo : "Bandejas Seladas",
+        descricao : "Qualidade, Segurança e Sustentabilidade.As Bandejas Seladas da linha PrisFood Cores da Pris Embalagens são a escolha ideal para quem busca eficiência e sustentabilidade no mercado de alimentos. Desenvolvidas para suportar temperaturas de até 180ºC, essas bandejas forneáveis oferecem uma alternativa superior às tradicionais marmitas de isopor e plástico, que são prejudiciais ao meio ambiente.Essas características tornam as Bandejas Seladas da Pris Embalagens uma escolha inteligente para inovar no setor de delivery de alimentos. Ofereça produtos de alta qualidade e sustentáveis aos seus clientes, confiando na Pris Embalagens para elevar o padrão do seu serviço.A melhor opção para suas refeições. As Box Antivazamento da Pris Embalagens são a solução perfeita para o seu serviço de delivery. Este produto combina sustentabilidade, versatilidade e segurança para proporcionar a melhor experiência tanto para você quanto para seus clientes. Feitas de material biodegradável, são uma excelente alternativa ao plástico e ao isopor, contribuindo para a preservação do meio ambiente. Opte por uma solução eco-friendly e promova práticas sustentáveis em seu negócio.Escolha entre os modelos com design padrão ou branco.",
+        link : "https://prisembalagens.com.br/produtos/bandeja-selada-linha-cores/",
+        tags : "selada base seladora restaurante"
+    
+    },
+    {
+        titulo : "Hamburgueria",
+        descricao : "A Pris Embalagens oferece uma variedade de embalagens para hamburguerias, priorizando a qualidade, sustentabilidade e design moderno. Seus produtos, como as box antivazamento, são perfeitos para manter os hambúrgueres quentinhos e suculentos, garantindo a satisfação dos seus clientes. Além disso, a empresa se preocupa com o meio ambiente e oferece opções biodegradáveis, contribuindo para um futuro mais sustentável. Com a Pris Embalagens, você encontra a embalagem ideal para elevar a experiência do seu cliente e fortalecer a identidade da sua marca.",
+        link : "https://prisembalagens.com.br/categoria/restaurantes-delivery-e-fast-foods/hamburguerias/",
+        tags : "burguer burger lanche lanchonete"
+    
+    }
+];
+
